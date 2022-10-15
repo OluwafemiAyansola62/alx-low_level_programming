ccf6b466c7c079d4af31ded284f8d3040827eccb
OluwafemiAyansola62 <oluwafemiayansola62@gmail.com>
2022-10-14 20:04:28 -0700

    ff

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stfio.h>
+/**
+ * main - numbers less than ten
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(( i % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
