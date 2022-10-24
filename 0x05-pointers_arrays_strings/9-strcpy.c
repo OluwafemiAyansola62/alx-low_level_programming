fb5b9d85f93e3020ace87652bc45130ab9cfc4a3
OluwafemiAyansola62 <oluwafemiayansola62@gmail.com>
2022-10-24 03:48:49 -0700

    string pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,9 @@ char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (*(src + index) != '\0')
 	{
-		dest[index] = src[index];
+		*(dest + index) = *(src + index);
 		index++;
 	}
 
