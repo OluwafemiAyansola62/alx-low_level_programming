f0ad0817555a0325ca301f8f4525a6f7040431ae
OluwafemiAyansola62 <oluwafemiayansola62@gmail.com>
2022-10-31 09:38:38 -0700

    moo

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,9 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
 	unsigned int index;
+
 	for (index = 0; index < n; index++)
 	{
-		destination[index] = source[index];
+		dest[index] = src[index];
 	}
 
 	return (dest);
