7abcde877614ed00c1df8a6385fb0da5b94710ea
OluwafemiAyansola62 <oluwafemiayansola62@gmail.com>
2022-11-10 13:46:58 -0800

    writting functions

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,7 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = old_ptr[i];
 	}
 	if (new_size < old_size)
-	}
+	{
 		for (i = 0; i < new_size; i++)
 		new_ptr[i] = old_ptr[i];
 	}
